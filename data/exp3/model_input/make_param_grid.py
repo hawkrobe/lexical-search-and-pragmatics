0be1901c39fac57a9acd93f11b595e983c62809e
Robert Hawkins <hawkrobe@gmail.com>
2023-12-21 23:22:30 -0600

    add basic infrastructure expected for exp3

--- data/exp3/model_input/make_param_grid.py
+++ data/exp3/model_input/make_param_grid.py
@@ -4,10 +4,10 @@ import numpy as np
 chainNum = 10000
 with open('grid.csv', 'w') as csv_file :
     writer = csv.writer(csv_file, delimiter=',')
-    for alpha in [2, 4, 8, 12, 16, 20, 25, 30, 36, 42, 50] :
-        for costWeight in [0, 0.01, 0.02, 0.04, 0.08, .16, .25, .32]:
+    for alpha in [2, 4, 8, 12, 16, 32] :
+        for costWeight in [0, 0.01, 0.02, 0.04, 0.08, .16, .32]:
             writer.writerow(['cdf', 'no_prag', alpha, round(costWeight,2), 0, chainNum])
-            for distractorWeight in [0, 0.01, 0.02, 0.04, 0.08, .16]:
+            for distractorWeight in [0, 0.001, 0.01, 0.02, 0.04]:
                 writer.writerow(['cdf', 'additive', alpha, round(costWeight,2), distractorWeight, chainNum])
                 writer.writerow(['freq', 'additive', alpha,  round(costWeight,2), distractorWeight, chainNum])
                 chainNum = chainNum + 1
