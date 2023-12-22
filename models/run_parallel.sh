34bc664ca665d303efaa817530219ccb9cc70e06
Robert Hawkins <hawkrobe@gmail.com>
2023-12-21 23:23:38 -0600

    remove optimize.py

--- models/run_parallel.sh
+++ models/run_parallel.sh
@@ -1,2 +1,2 @@
-# parallel --bar --colsep ',' "sh ./run_parallel.sh {1} {2} {3} {4} {5} {6}" :::: ../data/exp2/model_input//grid.csv
+# parallel --bar --colsep ',' "sh ./run_parallel.sh {1} {2} {3} {4} {5} {6}" :::: ../data/exp2/model_input/grid.csv
 python pragmatics.py $1 $2 $3 $4 $5 $6
